890d098eb3c4f0150991430dcf5ce32deb29a7c1
tori-xxi <78553786+tori-xxi@users.noreply.github.com>
2021-04-25 12:28:38 +0100

    Create validation.py

---
+++ validation.py
@@ -0,0 +1,15 @@
+def account_number_validation(account_number):
+
+    if account_number:
+
+        try:
+            int(account_number)
+
+            if len(str(account_number)) == 10:
+                return True
+
+        except ValueError:
+            return False
+        except TypeError:
+            return False
+    return False
